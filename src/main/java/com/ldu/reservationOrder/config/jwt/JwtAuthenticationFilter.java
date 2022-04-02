b8d4b1d8412c6548b458d1a4115a2374d55e32fa
lducoding <lducoding@gmail.com>
2022-04-02 23:02:10 +0900

    인증 완료 후 wjt 생성 중

--- src/main/java/com/ldu/reservationOrder/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ldu/reservationOrder/config/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.ldu.reservationOrder.config.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ldu.reservationOrder.config.auth.PrincipalDetails;
 import com.ldu.reservationOrder.dto.UserInfo;
@@ -10,9 +12,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -30,8 +35,25 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
             PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+            return authentication;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        String jwtToken = JWT.create()
+                .withSubject("lduToken") // 토큰 이름
+                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*10))) // 토큰 유효시간
+                .withClaim("id", principalDetails.getUserInfo().getId())
+                .withClaim("username", principalDetails.getUserInfo().getUsername())
+                .sign(Algorithm.HMAC512("donguking")); // 암호화 비밀키
+
+        response.addHeader("Authorization","Bearer "+jwtToken);
     }
 }
