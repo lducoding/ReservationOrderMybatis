ddbeee51310619214aee9aaec6635009151619a6
lducoding <lducoding@gmail.com>
2022-04-08 20:58:03 +0900

    톰캣8.5부터 쿠키값에 공백 불가

--- src/main/java/com/ldu/reservationOrder/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ldu/reservationOrder/config/jwt/JwtAuthenticationFilter.java
@@ -55,7 +55,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .sign(Algorithm.HMAC512("donguking")); // 암호화 비밀키
 
 //        response.addHeader("Authorization","Bearer "+jwtToken);
-        Cookie jwtCookie = new Cookie("Authorization", "Bearer "+jwtToken);
+        Cookie jwtCookie = new Cookie("Authorization", jwtToken);
         jwtCookie.setMaxAge(60*30);
         jwtCookie.setPath("/");
         jwtCookie.setSecure(true);
