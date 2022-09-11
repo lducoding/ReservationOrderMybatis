2b2b27510dbd15c63ba085e48a8b51b5862b9029
lducoding <lducoding@gmail.com>
2022-09-11 22:34:36 +0900

    has many 구현

--- src/main/java/com/ldu/reservationOrder/dto/RestaurantDetailDto.java
+++ src/main/java/com/ldu/reservationOrder/dto/RestaurantDetailDto.java
@@ -13,14 +13,11 @@ import java.util.Optional;
 public class RestaurantDetailDto {
 
     private Long restaurantId;
-//    private final List<Menu> menuList;
+    private List<Menu> menuList;
     private String location;
     private String restaurantName;
     private String category;
     private int standardTime;
     private GoalDto goalDto;
 
-//    private void setGoalDto(Long goalId, String goalType, int goalMoney, int recentMoney, String success, String registerDate) {
-//        this.goalDto = new GoalDto(goalId, goalType, goalMoney, recentMoney, success, registerDate);
-//    }
 }
